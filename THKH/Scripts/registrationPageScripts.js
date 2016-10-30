7a2c75f54d43c1dee75f6fb1c54c321f341e0657
abdulsr-2014 <abdulsr.2014@sis.smu.edu.sg>
2016-10-30 16:49:28 +0800

    completed a functioning example of a simple check in with ajax

--- THKH/Scripts/registrationPageScripts.js
+++ THKH/Scripts/registrationPageScripts.js
@@ -7,17 +7,18 @@ function callCheck (){
     
     alert(nric.value);
     //Do ajax call
-    var headersToProcess = { xAxis: xAxsList, yAxis: yAxsList, chartType: getChartObj.id };//Store objects in this manner 
+    var headersToProcess = {  messages: "Hi i was sent to and back" };//Store objects in this manner 
+
     $.ajax({
-        url: 'generateReport.ashx',
+        url: './CheckInOut/checkIn.ashx',
         method: 'post',
-        dataType: 'text',//JSON.parse
-        contentType: false,
-        processData: false,
+        data: headersToProcess,
+        
+        
         success: function (returner) {
             var resultOfGeneration = JSON.parse(returner);
-            if (resultOfGeneration.Result == "Error") {
-    
+            if (resultOfGeneration.Result == "Success") {
+                alert(resultOfGeneration.Msg);
             }
         },
         error: function (err) {
--- THKH/Webpage/Staff/CheckInOut/CheckIn.ashx.cs
+++ THKH/Webpage/Staff/CheckInOut/CheckIn.ashx.cs
@@ -14,10 +14,9 @@ namespace THKH.Webpage.Staff.CheckInOut
         public void ProcessRequest(HttpContext context)
         {   
             context.Response.ContentType = "text/plain";
-            var xAxisHeaders = context.Request.Form["xAxis"]; //Retrieve data in this manner
-            var yAxisHeaders = context.Request.Form["yAxis"];
-            var chartType = context.Request.Form["chartType"];
-            String successString = "{\"Result\":\"Success\",\"Path\":\"\"}";
+         
+            var msgg = context.Request.Form["messages"];
+            String successString = "{\"Result\":\"Success\",\"Msg\":\""+ msgg + "\"}";
             context.Response.Write(successString);//Json Format
         }
 
